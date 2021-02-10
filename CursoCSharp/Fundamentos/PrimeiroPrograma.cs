01942ca5d8c0425b592f5b6ec0c0d4c9e25ec41c
Gabriel Rodrigues Souza <rsgabriel01@gmail.com>
2021-02-10 18:49:23 -0300

    Initial files of course

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos {
+  class PrimeiroPrograma {
+    static void Executar() {
+      Console.Write("Primeiro ");
+      Console.WriteLine("Programa");
+      Console.WriteLine("Teminou!");
+    }
+  }
+}
